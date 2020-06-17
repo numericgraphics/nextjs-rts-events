0c8e2062f7a1f38bfc508102706b0dce5e4edc37
Rinaldo <contact@numericgraphics.com>
2020-06-17 18:22:03 +0200

    add all templates for promos

--- components/promos/promoImage.js
+++ components/promos/promoImage.js
@@ -1,75 +1,46 @@
 import React, { useEffect } from 'react'
 import Box from '@material-ui/core/Box'
-import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
 
 const styles = {
-    slide: {
+    containerOverlay: {
+        position: 'absolute',
         display: 'flex',
         flexDirection: 'column',
-        minHeight: '100vh',
-        zIndex: 0,
-        backgroundColor: 'gray'
-    },
-    slideGradient: {
-        position: 'absolute',
+        justifyContent: 'center',
         width: '100vw',
-        height: '70vh',
-        flexGrow: 1,
-        zIndex: 1,
-        background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0) 35%, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%)'
-    },
-    slideTitle: {
-        width: '100%',
-        minHeight: '10vh',
-        alignSelf: 'center',
-        alignContent: 'center',
-        textAlign: 'center',
-        fontSize: '2rem',
-        color: 'green',
-        paddingBottom: 20
-    },
-    slideTitleTypo: {
-        fontFamily: 'srgssr-type-BdIt',
-        lineHeight: 1,
-        padding: 10,
-        backgroundColor: '#409AD3',
-        color: 'white'
-    },
-    slideBody: {
-        position: 'absolute',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        minHeight: '40vh',
-        zIndex: 2
-    },
-    slideBodyIOS: {
-        position: 'absolute',
-        top: '30vh',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        minHeight: '35vh',
-        zIndex: 2
-    },
-    slideTeaser: {
-        fontFamily: 'srgssr-type-Bd',
-        color: 'white'
+        minHeight: '100vh',
+        backgroundColor: 'pink'
     }
 }
 
+const useStyles = makeStyles({
+    containerGlobal: {
+        width: '100vw',
+        minHeight: '100vh',
+        backgroundColor: 'pink'
+    },
+    gradient: {
+        position: 'absolute',
+        width: '100vw',
+        height: '100vh',
+        flexGrow: 1,
+        zIndex: 1,
+        background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0) 35%, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%)'
+    }
+})
+
 export default function PromoImage (props) {
+    const classes = useStyles()
+    const { backgroundImageURL } = props.data
     useEffect(() => {
 
     }, [props.selected])
 
     return (
-        <Box>
-            <Typography style={styles.slideTeaser} variant="h5" align={'center'}>PromoImage</Typography>
+        <Box className={classes.containerGlobal}>
+            <Box className={classes.gradient}/>
+            <Box style={{ ...styles.containerOverlay, backgroundImage: `url(${backgroundImageURL})`, backgroundPosition: 'center' }} />
         </Box>
     )
 }
--- components/promos/promoLogo.js
+++ components/promos/promoLogo.js
@@ -1,75 +1,71 @@
 import React, { useEffect } from 'react'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import CardMedia from '@material-ui/core/CardMedia'
 
 const styles = {
-    slide: {
+    containerOverlay: {
+        position: 'absolute',
         display: 'flex',
         flexDirection: 'column',
+        justifyContent: 'center',
+        width: '100vw',
         minHeight: '100vh',
-        zIndex: 0,
-        backgroundColor: 'gray'
+        backgroundColor: 'pink'
+    }
+}
+
+const useStyles = makeStyles({
+    containerGlobal: {
+        width: '100vw',
+        minHeight: '100vh',
+        backgroundColor: 'pink'
     },
-    slideGradient: {
+    gradient: {
         position: 'absolute',
         width: '100vw',
-        height: '70vh',
+        height: '100vh',
         flexGrow: 1,
         zIndex: 1,
         background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%,rgba(0,0,0,0) 35%, rgba(0,0,0,0) 70%, rgba(0,0,0,1) 100%)'
     },
-    slideTitle: {
-        width: '100%',
-        minHeight: '10vh',
+    text: {
+        padding: 10
+    },
+    image: {
         alignSelf: 'center',
-        alignContent: 'center',
-        textAlign: 'center',
-        fontSize: '2rem',
-        color: 'green',
-        paddingBottom: 20
-    },
-    slideTitleTypo: {
-        fontFamily: 'srgssr-type-BdIt',
-        lineHeight: 1,
-        padding: 10,
-        backgroundColor: '#409AD3',
-        color: 'white'
-    },
-    slideBody: {
-        position: 'absolute',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        minHeight: '40vh',
-        zIndex: 2
-    },
-    slideBodyIOS: {
-        position: 'absolute',
-        top: '30vh',
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'flex-end',
-        alignItems: 'center',
-        minHeight: '35vh',
-        zIndex: 2
-    },
-    slideTeaser: {
-        fontFamily: 'srgssr-type-Bd',
-        color: 'white'
+        padding: 20,
+        maxWidth: 400
     }
-}
+})
 
 export default function PromoLogo (props) {
+    const classes = useStyles()
+    const { description, title, backgroundImageURL, logoURL } = props.data
     useEffect(() => {
 
     }, [props.selected])
 
     return (
-        <Box>
-            <Typography style={styles.slideTeaser} variant="h5" align={'center'}>PromoLogo</Typography>
+        <Box className={classes.containerGlobal}>
+            <Box className={classes.gradient}/>
+            <Box style={{ ...styles.containerOverlay, backgroundImage: `url(${backgroundImageURL})`, backgroundPosition: 'center' }} >
+
+                <Box className={classes.image}>
+                    <CardMedia
+                        component="img"
+                        alt="Contemplative Reptile"
+                        image={logoURL}
+                        title="Contemplative Reptile"
+                    />
+                </Box>
+                <Box className={classes.text}>
+                    <Typography variant="h5" align={'center'}>{title}</Typography>
+                    <Typography variant="body1" align={'center'}>{description}</Typography>
+                </Box>
+            </Box>
         </Box>
+
     )
 }
