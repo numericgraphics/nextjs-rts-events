7bbdbe09619bc64fe4f0fc623ecaac1834e0c09f
Rinaldo <contact@numericgraphics.com>
2020-07-15 14:42:26 +0200

    add image in question page, with gradient, and timer icon

--- components/questionTimer.js
+++ components/questionTimer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import LinearProgress from '@material-ui/core/LinearProgress'
+import TimerIcon from '@material-ui/icons/Timer'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
@@ -7,8 +8,26 @@ import { makeStyles } from '@material-ui/core/styles'
 const useStyles = makeStyles({
     container: {
         width: '100vw'
+    },
+    content: {
+        display: 'flex',
+        flexDirection: 'row',
+        paddingTop: 5
+    },
+    text: {
+        fontFamily: 'srgssr-type-Rg',
+        fontSize: '1rem',
+        color: '#A9A9A9',
+        textShadow: '0px 3px 3px #00000040',
+        paddingLeft: 5
     }
 })
+const styles = {
+    icon: {
+        color: '#A9A9A9',
+        textShadow: '0px 3px 3px #00000040'
+    }
+}
 
 function QuestionTimer (props) {
     const classes = useStyles()
@@ -17,8 +36,9 @@ function QuestionTimer (props) {
     return (
         <Box className={classes.container}>
             <LinearProgress variant="determinate" value={progress} />
-            <Box minWidth={35}>
-                <Typography variant="body2" color="textSecondary">
+            <Box className={classes.content}>
+                <TimerIcon style={styles.icon}/>
+                <Typography className={classes.text} >
                     {`${Math.round(timeLeft)} secondes restantes`}
                 </Typography>
             </Box>
