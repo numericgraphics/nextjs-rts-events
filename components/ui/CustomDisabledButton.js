46e9fa61752308dffb84019883f3a37bca629651
Rinaldo <contact@numericgraphics.com>
2020-07-23 14:15:18 +0200

    fine-tuning - buttons question disabled

--- components/ui/CustomDisabledButton.js
+++ components/ui/CustomDisabledButton.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button'
 
 export const CustomDisabledButton = withStyles(() => ({
     disabled: {
-        color: 'rgba(0, 0, 0, 0.26)!important',
-        backgroundColor: 'rgba(62, 62, 62, 0.6)!important'
+        color: '#777777!important',
+        backgroundColor: '#444444!important'
     }
 }))(Button)
