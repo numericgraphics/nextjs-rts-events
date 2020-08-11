1ac2f490267647aabf5553b4a6b5464b970f493a
Mathieu Mesquita <mathieumesquita@MacBook-Pro-de-Mathieu.local>
2020-08-11 16:44:26 +0200

    Fixed problem with file

--- components/UserContext 2.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from 'react'
-
-const UserContext = createContext(undefined, undefined)
-
-export default UserContext
--- components/ui/ColorCardContent 2.js
+++
@@ -1,8 +0,0 @@
-import withStyles from '@material-ui/core/styles/withStyles'
-import CardContent from '@material-ui/core/CardContent'
-
-export const ColorCardContent = withStyles(() => ({
-    root: {
-        minHeight: '250px!important'
-    }
-}))(CardContent)
