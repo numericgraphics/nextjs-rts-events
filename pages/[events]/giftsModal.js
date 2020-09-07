f7fe3baa85fb138f4bc27f7fc17742ec8ded2af9
Mathieu Mesquita <mathieumesquita@MacBook-Pro-de-Mathieu.local>
2020-09-07 15:16:01 +0200

    Fix image display v2

--- pages/[events]/giftsModal.js
+++ pages/[events]/giftsModal.js
@@ -159,7 +159,6 @@ const hasLoginModal = WrappedComponent => {
                 // eslint-disable-next-line no-unused-expressions
             }
             // eslint-disable-next-line no-unused-expressions
-            handleResize()
             window.addEventListener('resize', handleResize)
         }, [boxTextRef.current])
 
