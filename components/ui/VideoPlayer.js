fa42aef1f5d991bcc3f0f33fb07033476eb2b10d
Rinaldo <contact@numericgraphics.com>
2020-09-07 15:12:27 +0200

    add poster

--- components/ui/VideoPlayer.js
+++ components/ui/VideoPlayer.js
@@ -1,6 +1,7 @@
 import React, { forwardRef } from 'react'
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import Fade from '@material-ui/core/Fade/Fade'
+import Box from '@material-ui/core/Box'
 
 const useStyles = makeStyles({
     ContainerVideo: {
@@ -15,28 +16,43 @@ const useStyles = makeStyles({
     playBtn: {
         display: 'none',
         opacity: 0
+    },
+    poster: {
+        zIndex: 1,
+        position: 'absolute',
+        width: '100vw',
+        minHeight: '100vh',
+        top: 0,
+        left: 0,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'top',
+        backgroundSize: 'cover',
+        backgroundColor: 'black'
     }
 })
 
 function VideoPlayer (props, videoRef) {
     const classes = useStyles()
     const theme = useTheme()
-    const { videoSource, videoPoster, showVideo, blurVideo } = props
+    const { videoSource, videoPoster, showVideo, blurVideo, videoHasPlayed } = props
 
     return (
         <Fade in={showVideo} timeout={500}>
-            <video
-                ref={videoRef}
-                preload={'auto'}
-                src={videoSource}
-                loop
-                playsInline
-                poster={videoPoster}
-                className={classes.video}
-                autoPlay
-                style={{ backgroundColor: theme.palette.background, filter: blurVideo ? 'blur(4px)' : 'none' }}
-            >
-            </video>
+            <Box>
+                <Box className={classes.poster} style={{ backgroundImage: `url(${videoPoster})`, display: videoHasPlayed ? 'none' : 'box' }} />
+                <video
+                    ref={videoRef}
+                    preload={'auto'}
+                    src={videoSource}
+                    loop
+                    playsInline
+                    poster={videoPoster}
+                    className={classes.video}
+                    autoPlay
+                    style={{ backgroundColor: theme.palette.background, filter: blurVideo ? 'blur(4px)' : 'none' }}
+                >
+                </video>
+            </Box>
         </Fade>
     )
 }
