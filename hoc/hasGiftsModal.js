2c310f399e5705a43f7dbb3694b4eaa0a9944804
mathieu.mesquita@rts.ch <mathieumesquita@MacBook-Pro-de-Mathieu.local>
2020-09-09 14:52:03 +0200

    Fix refactoring

--- hoc/hasGiftsModal.js
+++ hoc/hasGiftsModal.js
@@ -108,16 +108,6 @@ const useStyles = makeStyles(() => ({
         maxWidth: '58px',
         width: '10vw',
         height: '10vw'
-    },
-    image: {
-        position: 'absolute',
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center',
-        backgroundSize: 'auto 100%',
-        width: '100vw',
-        backgroundColor: 'white',
-        minHeight: '100vh'
-
     }
 }))
 
@@ -186,7 +176,7 @@ const hasGiftModal = WrappedComponent => {
                                 <Typography className={classes.description} variant="h4" align={'center'}>{gift.locked ? gift.lockedMessage : gift.message}</Typography>
                             </Box>
                             <Box className={classes.gradient} style={{ background: `linear-gradient(to top, ${theme.palette.secondary.main} 10%,${theme.palette.secondary.main + '00'} 100%)`, marginBottom: boxHeight - 1 }} />
-                            <LazyImage className={classes.image} style={{ backgroundImage: `url(${gift.imageURL})` }}/>
+                            <LazyImage style={{ position: 'absolute', backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'auto 100%', width: '100vw', backgroundColor: 'white', backgroundImage: `url(${gift.imageURL})`, minHeight: '100vh' }}/>
                             <IconButton onClick={handleClose} color="primary" className={classes.closeBtn} style={{ backgroundColor: theme.palette.primary.main, stroke: theme.palette.primary.contrastText }}>
                                 { closeIcon({ className: classes.closeIcon }) }
                             </IconButton>
