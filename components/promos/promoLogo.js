508d7d1c4a59c85c1ce766f553fd8a3d6c6d516c
Rinaldo <contact@numericgraphics.com>
2020-09-10 15:48:36 +0200

    fix css import

--- components/promos/promoLogo.js
+++ components/promos/promoLogo.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Box from '@material-ui/core/Box'
 import Typography from '@material-ui/core/Typography'
 import CardMedia from '@material-ui/core/CardMedia'
-import { useStyles } from '../../styles/promo-style.js'
+import { useStyles } from '../../styles/promo-style'
 import { useHeight } from '../../hooks/useHeight'
 
 export default function PromoLogo (props) {
@@ -23,8 +23,8 @@ export default function PromoLogo (props) {
                         />
                     </Box>
                     <Box className={styles.text}>
-                        <Typography className={styles.title} variant="h1" align={'center'}>{title}</Typography>
-                        <Typography className={styles.subTitle} variant="h3" align={'center'}>{description}</Typography>
+                        <Typography className={styles.title} align={'center'}>{title}</Typography>
+                        <Typography className={styles.subTitle} align={'center'}>{description}</Typography>
                     </Box>
                 </Box>
             </Box>
