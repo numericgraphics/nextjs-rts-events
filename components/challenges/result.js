682b9e83e28fbadf6692d6949e567a86a283aeb1
mathieu.mesquita@rts.ch <mathieu.mesquita@rts.ch>
2020-09-18 18:02:53 +0200

    Fix font

--- components/challenges/result.js
+++ components/challenges/result.js
@@ -37,7 +37,7 @@ const useStyles = makeStyles((theme = useTheme()) => ({
     },
     subTitle: {
         fontFamily: 'srgssr-type-Rg',
-        fontSize: '1rem',
+        fontSize: '1.2rem',
         textAlign: 'center',
         lineHeight: 1,
         marginBottom: 100,
