1771e4ba5b305cee19865d385f2f3d27d6fccce3
Rinaldo <contact@numericgraphics.com>
2020-09-22 10:58:47 +0200

    add meta viewport

--- components/eventLayout.js
+++ components/eventLayout.js
@@ -20,6 +20,7 @@ export default function EventLayout ({ children, home }) {
         <div className={'container'} >
             <Head>
                 <link rel="icon" href="/favicon.ico" />
+                < meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
                 <meta
                     name="description"
                     content="TODO"
