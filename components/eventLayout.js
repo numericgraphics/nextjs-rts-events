c4939c7cc6474a9e4420b2cff728bb1ac46bf082
Rinaldo <contact@numericgraphics.com>
2020-10-14 14:35:46 +0200

    add favicon, and meta description

--- components/eventLayout.js
+++ components/eventLayout.js
@@ -23,7 +23,7 @@ export default function EventLayout ({ children, home }) {
                 < meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"/>
                 <meta
                     name="description"
-                    content="TODO"
+                    content="RTS Events"
                 />
             </Head>
             {
