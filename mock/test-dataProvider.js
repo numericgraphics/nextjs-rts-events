7c9340fed5f9611dc58ceead8789d1fba56dc877
mathieu.mesquita@rts.ch <mathieu.mesquita@rts.ch>
2020-10-23 12:34:50 +0200

    Adding test on dataProvider

---
+++ mock/test-dataProvider.js
@@ -0,0 +1,33 @@
+export default {
+    event: {
+        shortName: 'WF2',
+        lang: 'fr',
+        baseURL: 'https://web-front-v3-git-feature-global-state.rtsch.now.sh',
+        promos: [{
+            backgroundImageURL: 'https://cdn.rts.ch/rtschallengeassets/events/qq/startPageQQ.jpg',
+            description: "Élargissez le cercle, les femmes et les jeunes blaguaient la grosse tête, barbu, vêtu d'une livrée bleu de ciel par-dessus cet habit for…",
+            logoURL: 'https://cdn.rts.ch/rtschallengeassets/events/qq/logoQQ.png',
+            title: 'Logo with Title and description',
+            type: 'Logo'
+        }],
+        startPageElements: [{
+            backgroundImageURL: 'https://cdn.unitycms.io/image/ocroped/2001,2000,408,760,90,82/BrbVjH2I0Tc/5L7i2emn4f_BDJIIdAeZIM.jpg',
+            description: "Et qu'est-ce qui se passe avec un fond TRES clair?",
+            layout: 'Promo',
+            logoURL: null,
+            title: 'Possibilité de mettre un titre en dynamique sur plusieurs lignes.'
+        }]
+    },
+    gameStats: { totalChallengesCount: 32, availableChallengesCount: 25, activeChallengesCount: 32, completedActiveChallengesCount: 7, completedChallengesCount: 7 },
+    gifts: [{
+        description: "DESCRIPTION: Badge d'honneur de 1er niveau.",
+        giftID: 'g0',
+        imageURL: 'https://cdn.rts.ch/rtschallengeassets/gifts/WF/Cadeau2%402x.jpg',
+        locked: false,
+        message: 'Bravo, vous êtes inscrit au tirage au sort. Le gagnant sera averti par SMS.',
+        title: '(a 33 points)<br>Tirage au sort pour gagner deux billets pour New-York!',
+        type: 'lottery'
+    }],
+    uiElements: { successChunk: '2', failChunk: '5', sumChunk: '<strong>7</strong> défis joués', scoreChunk: '400 pts.', resultTitleChunk: null },
+    user: { nickname: 'chat', avatarURL: 'https://cdn.rts.ch/rtschallengeassets/avatars/qq/c' }
+}
--- tests/dataProvider.test.js
+++ tests/dataProvider.test.js
@@ -1,18 +1,40 @@
 import DataProvider from '../data/dataProvider'
+import data from '../mock/test-dataProvider'
 
 test('get translation', () => {
+    DataProvider.setData(data)
     const translation = DataProvider.getTranslation()
     expect(translation).toBeDefined()
 })
 
-test('get start page element', () => {
-    const startElements = DataProvider.getStartPageElements()
-    expect(startElements).toBeDefined()
+test('check if successChunk = 2', () => {
+    const uiElements = DataProvider.getUiElements()
+    expect(uiElements.successChunk).toContain('2')
 })
 
-test('get UiElements', () => {
+test('set successChunk = 10', () => {
+    DataProvider.setData({ uiElements: { successChunk: '10' } })
+    expect(DataProvider.data.uiElements.successChunk).toContain('10')
+})
+
+test('add UiElement and reset all other : giftBarProgression', () => {
+    DataProvider.setData({ uiElements: { giftBarProgression: '90' } })
+    expect(DataProvider.data.uiElements.giftBarProgression).toContain('90')
+    expect(DataProvider.data.uiElements.successChunk).toBeUndefined()
+})
+
+test('add UiElement : giftEnd', () => {
     const uiElements = DataProvider.getUiElements()
-    expect(uiElements).toBeDefined()
+    DataProvider.setData({ uiElements: { ...uiElements, giftEnd: '2021' } })
+    console.log('dataProvider', DataProvider)
+    expect(DataProvider.data.uiElements.giftEnd).toContain('2021')
+    expect(DataProvider.data.uiElements.giftBarProgression).toBeDefined()
+})
+
+test('reset uiElements', () => {
+    DataProvider.setData({ uiElements: {} })
+    const undefinedObject = {}
+    expect(DataProvider.data.uiElements).toMatchObject(undefinedObject)
 })
 
 test('get gameStat', () => {
