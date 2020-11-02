851381b4a12e5b9428debda4ed6b1b5991853926
Rinaldo <contact@numericgraphics.com>
2020-11-02 15:52:31 +0100

    improve themeFactory

--- styles/global.style.js
+++ styles/global.style.js
@@ -1,7 +1,6 @@
 import { makeStyles, useTheme } from '@material-ui/core/styles'
 import { hexToRGBA } from '../data/tools'
 
-
 export const useStylesGlobal = makeStyles((theme = useTheme) => ({
     bottomZoneGradient: {
         background: 'linear-gradient(to bottom, ' + hexToRGBA(theme.palette.secondary.main, 0) + ' 0%,  ' + hexToRGBA(theme.palette.secondary.main, 0.7) + ' 20%, ' + hexToRGBA(theme.palette.secondary.main, 1) + ' 100%)',
