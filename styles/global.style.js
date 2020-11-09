7e78a29e5946a83b751aa6fcbe6faa741a52b5a2
Gosteli Nathanaël <nathanael.gosteli@rts.ch>
2020-11-09 17:05:11 +0100

    background moins opaque

--- styles/global.style.js
+++ styles/global.style.js
@@ -8,7 +8,7 @@ export const useStylesGlobal = makeStyles((theme = useTheme) => ({
     },
     colorOverImage: {
         backgroundColor: theme.palette.primary.main,
-        opacity: 0.9
+        opacity: 0.7
     },
     backdropFilterOverImage: {
         backdropFilter: 'blur(0.3rem)' // Chrome, edge, safari, ... https://caniuse.com/css-backdrop-filter
