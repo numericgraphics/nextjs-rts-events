288e2726980f15ef37373ac87d3c8e1d85a62bd8
Rinaldo <contact@numericgraphics.com>
2020-11-11 16:44:16 +0100

    fix progress bg color

--- components/progress.js
+++ components/progress.js
@@ -14,7 +14,6 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
         alignContent: 'center',
         justifyContent: 'center',
-        backgroundColor: theme.palette ? theme.palette.primary.main : ThemeFactory.getDefaultTheme().palette.primary.main,
         overflow: 'hidden'
     }
 }))
@@ -31,7 +30,7 @@ function Progress () {
 
     return (
         <Box className={'container'}>
-            <Box className={[classes.container, 'background'].join(' ')} style={{ height: height }}>
+            <Box className={[classes.container, 'background'].join(' ')} style={{ height: height, backgroundColor: theme.palette ? theme.palette.primary.main : ThemeFactory.getDefaultTheme().palette.primary.main }}>
                 <CircularProgress style={{ color: theme.palette ? theme.palette.primary.contrastText : ThemeFactory.getDefaultTheme().palette.primary.contrastText }} />
             </Box>
         </Box>
