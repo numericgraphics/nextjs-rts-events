6a059d262e522489866f5d5d9114fa1295b36081
mathieu.mesquita@rts.ch <mathieu.mesquita@rts.ch>
2021-01-15 16:02:03 +0100

    Add test on b64Conv

--- tests/tools.test.js
+++ tests/tools.test.js
@@ -5,9 +5,11 @@ import {
     phoneVerification,
     dateCreator,
     epochConverter,
-    between
+    between,
+    b64Conv
 } from '../data/tools'
 import data from '../mock/test-call-event'
+// const img = require('file!./test.png')
 
 test('get images from JSON using getAllImagesFromJSON function', () => {
     const images = getAllImagesFromJSON(data, [], ['.jpg', '.png', '.jpeg', '.image?'])
@@ -61,4 +63,22 @@ test('between', () => {
     expect(testBetweenTwo).toBe(true)
     const testBetweenThree = between(2.5, 20, 30)
     expect(testBetweenThree).toBe(false)
+})
+test('b64Conv', async () => {
+    const base64 = 'iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAABJlBMVEUAAADjARXkABTkAhjkARzlABzlAh/kABzjARTjABPkAhflAR3lAB3lABrlABzkABrlAiDkABvnHDbziJj0k6Hxd4jpLEXyfI71lKL0kqH0j5/rOlLvZXj1m6jzipnnGTTlAR7qNEr74OTygpD4xczuWG73sr3609nwaXzwcIL62+Dwc4PuXG7mECzlAR/609jsRVj3vcTxfYzkABXydof3sLvlARvrPlP62d70nqnsTWDlBiL60dbxeYj73uLpMEfkABfyd4n4sLzlBCLkAx7qPFDzjZr73+TrQljkABzpNEr61NnqOlD3vsbyh5XlABv3sbzpKkPwdYPyhZH62t/rQVfnGjPwanzmESvqPFPwaX3mCSbrOlPqMUrziZjtUmblAh////9SFQjJAAAAC3RSTlMAUlJS/Pz8/FJSUoV0BZ0AAAABYktHRGGysEyGAAAAB3RJTUUH5AsXDA4BG4FJfwAAAKVJREFUGNNjYCAIGJlQADMDCysKYGPg5uHh4eXj4eHj4eMHMgUYBIWERUTFxCUkpaRlZOXkFRj4FZWUVURU1dQ1NLW0dXT1gAL6BoZGxiamZuYWllbWAkABG1s7ewdHJ2cXVzd3D08Gfi9vH18/f8cAgcCg4JBQTwbPsPCIyKjoGFWe2Li4eOsEoLWCPP7+PHxAxM/PD7SWhR0FsDFwcKIALsKeBQBtAxfPfnNW7QAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAyMC0xMS0yM1QxMjoxNDowMSswMTowMGgQvo8AAAAldEVYdGRhdGU6bW9kaWZ5ADIwMjAtMTEtMjNUMTI6MTQ6MDErMDE6MDAZTQYzAAAAV3pUWHRSYXcgcHJvZmlsZSB0eXBlIGlwdGMAAHic4/IMCHFWKCjKT8vMSeVSAAMjCy5jCxMjE0uTFAMTIESANMNkAyOzVCDL2NTIxMzEHMQHy4BIoEouAOoXEXTyQjWVAAAAAElFTkSuQmCC'
+    function fixBinary (bin) {
+        var length = bin.length
+        var buf = new ArrayBuffer(length)
+        var arr = new Uint8Array(buf)
+        for (var i = 0; i < length; i++) {
+            arr[i] = bin.charCodeAt(i)
+        }
+        return buf
+    }
+    var binary = fixBinary(atob(base64))
+    console.log(binary)
+    var blob = new Blob([binary], { type: 'image/png' })
+    const res = await b64Conv(blob)
+    expect(res).toMatch('data:image/png;base64,' + base64)
+    // b64Conv(blob).then(data => expect(data).toReturn(base64))
 })
