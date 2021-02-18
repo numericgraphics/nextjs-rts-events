634f2b9bb5f4df606c3734620777006e272ad3cc
mathieu.mesquita@rts.ch <mathieu.mesquita@rts.ch>
2021-02-18 17:40:01 +0100

    Working v1

--- components/map/MainMap.js
+++ components/map/MainMap.js
@@ -1,148 +1,168 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import Box from '@material-ui/core/Box'
 import WebMap from '@arcgis/core/Map'
 import MapView from '@arcgis/core/views/MapView'
-import Layer from '@arcgis/core/layers/Layer'
 import Locate from '@arcgis/core/widgets/Locate'
 import LayerList from '@arcgis/core/widgets/LayerList'
 import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer'
 import '@arcgis/core/assets/esri/themes/dark/main.css'
 import config from '@arcgis/core/config.js'
+import Router, { useRouter } from 'next/router'
 config.assetsPath = '/assets'
 
 function MainMap (props) {
-    const baseURL = window.location.origin
+    const router = useRouter()
     const mapDiv = useRef(null)
-    useEffect(() => {
-        if (mapDiv.current) {
-            const popupTemplate = {
-                // autocasts as new PopupTemplate()
-                title: '{titre}',
-                content: [
+    const [layers, setLayers] = useState()
+    const { events } = router.query
+
+    const renderer = {
+        type: 'simple',
+        field: 'mag',
+        symbol: {
+            type: 'simple-marker',
+            size: 7,
+            color: '#69dcff',
+            outline: {
+                color: 'rgba(0, 139, 174, 0.5)',
+                width: 8
+            }
+        }
+    }
+
+    const cluster = {
+        type: 'cluster',
+        clusterRadius: '100px',
+        // {cluster_count} is an aggregate field containing
+        // the number of features comprised by the cluster
+        popupTemplate: {
+            // Desactiver le zoom overwriteActions: false,
+            content: '{cluster_count} défi(s) ont été réalisé dans cette region. Zoom pour en voir plus !',
+            fieldInfos: [
+                {
+                    fieldName: 'cluster_count',
+                    format: {
+                        places: 0,
+                        digitSeparator: true
+                    }
+                }
+            ]
+        },
+        clusterMinSize: '32px',
+        clusterMaxSize: '60px',
+        labelingInfo: [
+            {
+                deconflictionStrategy: 'none',
+                labelExpressionInfo: {
+                    expression: "Text($feature.cluster_count, '#,###')"
+                },
+                symbol: {
+                    type: 'text',
+                    color: '#004a5d',
+                    font: {
+                        weight: 'bold',
+                        family: 'Noto Sans',
+                        size: '12px'
+                    }
+                },
+                labelPlacement: 'center-center'
+            }
+        ]
+    }
+
+    const popupTemplate = {
+        // autocasts as new PopupTemplate()
+        title: '{titre}',
+        content: [
+            {
+                // It is also possible to set the fieldInfos outside of the content
+                // directly in the popupTemplate. If no fieldInfos is specifically set
+                // in the content, it defaults to whatever may be set within the popupTemplate.
+                type: 'fields',
+                fieldInfos: [
                     {
-                        // It is also possible to set the fieldInfos outside of the content
-                        // directly in the popupTemplate. If no fieldInfos is specifically set
-                        // in the content, it defaults to whatever may be set within the popupTemplate.
-                        type: 'fields',
-                        fieldInfos: [
-                            {
-                                fieldName: 'eventDescription',
-                                label: 'Description'
-                            },
-                            {
-                                fieldName: 'eventDate',
-                                label: 'Date'
-                            },
-                            {
-                                fieldName: 'eventHour',
-                                label: 'Heure'
-                            }
-                        ]
+                        fieldName: 'nickname',
+                        label: 'Utilisateur :'
                     }
                 ]
-            }
-
-            const renderer = {
-                type: 'simple',
-                field: 'mag',
-                symbol: {
-                    type: 'simple-marker',
-                    size: 10,
-                    color: '#019406',
-                    outline: {
-                        color: 'rgba(1, 148, 6, 0.5)',
-                        width: 11
+            },
+            {
+                // The following creates a piechart in addition to an image. The chart is
+                // also set  up to work with related tables.
+                // Autocasts as new MediaContent()
+                type: 'media',
+                // Autocasts as array of MediaInfo objects
+                mediaInfos: [{
+                    title: '{imageTitre}',
+                    type: 'image', // Autocasts as new ImageMediaInfo object
+                    // caption: "tree species",
+                    // Autocasts as new ImageMediaInfoValue object
+                    value: {
+                        sourceURL: '{imageURL}'
                     }
-                }
+                }]
             }
+        ]
+    }
 
-            const pastRenderer = {
-                type: 'simple',
-                field: 'mag',
-                symbol: {
-                    type: 'simple-marker',
-                    size: 10,
-                    color: '#af001e',
-                    outline: {
-                        color: 'rgba(175, 0, 30, 0.5)',
-                        width: 11
-                    }
-                }
+    async function fetchData () {
+        try {
+            const bodyContent = { eventName: events }
+            const response = await fetch('/api/fetchGeoJson', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(bodyContent)
+            })
+            if (response.status === 200) {
+                const content = await response.json()
+                console.log(content)
+                // initGame(content)
+                getFinalLay(content)
+            } else {
+                await Router.push('/[events]', {
+                    pathname: `/${events}`,
+                    query: { modal: true }
+                })
             }
+        } catch (error) {
+            throw new Error(error.message)
+        }
+    }
 
-            const futurRenderer = {
-                type: 'simple',
-                field: 'mag',
-                symbol: {
-                    type: 'simple-marker',
-                    size: 10,
-                    color: '#e3cc00',
-                    outline: {
-                        color: 'rgba(227, 204, 0, 0.5)',
-                        width: 11
-                    }
-                }
+    async function getFinalLay (layersJson) {
+        var final = []
+        if (layersJson.geoJSONList) {
+            for (const layer in layersJson.geoJSONList) {
+                console.log(layer)
+                const newLayer = new GeoJSONLayer({
+                    url: 'https://zhihvqheg7.execute-api.eu-central-1.amazonaws.com/latest' + layersJson.geoJSONList[layer].relativeURL,
+                    title: layersJson.geoJSONList[layer].title,
+                    featureReduction: cluster,
+                    renderer: renderer, // optional,
+                    popupTemplate
+                })
+                console.log(newLayer.url)
+                // webmap.add(newLayer)
+                final.push(newLayer)
             }
+        }
+        setLayers(final)
+    }
 
-            const dayLayer = new GeoJSONLayer({
-                url: baseURL + '/currentEvent.geojson',
-                title: 'Event du jour',
-                renderer: renderer, // optional,
-                popupTemplate
-            })
-
-            const futurLayer = new GeoJSONLayer({
-                url: baseURL + '/futurEvent.geojson',
-                visible: false,
-                title: 'Event à venir',
-                renderer: futurRenderer, // optional,
-                popupTemplate
-            })
-
-            const pastLayer = new GeoJSONLayer({
-                url: baseURL + '/pastEvent.geojson',
-                visible: false,
-                title: 'Event passé',
-                copyright: 'RTS Challenge',
-                renderer: pastRenderer, // optional,
-                popupTemplate
-            })
-
-            /* geojsonLayer.popupTemplate.content = [{
-          // The following creates a piechart in addition to an image. The chart is
-          // also set  up to work with related tables.
-          // Autocasts as new MediaContent()
-          type: "media",
-          // Autocasts as array of MediaInfo objects
-          mediaInfos: [{
-            title: "<b>Mexican Fan Palm</b>",
-            type: "image", // Autocasts as new ImageMediaInfo object
-            caption: "tree species",
-            // Autocasts as new ImageMediaInfoValue object
-            value: {
-              sourceURL: "https://www.sunset.com/wp-content/uploads/96006df453533f4c982212b8cc7882f5-800x0-c-default.jpg"
-            }
-          }]
-        }]; */
-            // test
-            /* var basemap = new Basemap({
-          baseLayers: [
-            new MapImageLayer({
-              url: "url to your dynamic MapServer",
-              title: "Basemap"
-            })
-          ],
-          title: "basemap",
-          id: "basemap"
-        }); */
-
+    useEffect(() => {
+        if (!layers) {
+            fetchData()
+        } else if (mapDiv.current) {
             const webmap = new WebMap({
-                basemap: 'topo-vector',
-                // basemap: basemap,
-                layers: [futurLayer, dayLayer, pastLayer]
+                basemap: {
+                    portalItem: {
+                        id: 'e3a50650b40e45a9b7071c72019e87b4',
+                        portal: 'https://www.arcgis.com'
+                    }
+                },
+                layers: layers
+                // layers: dayLayer
             })
-
-            console.log(webmap)
 
             const view = new MapView({
                 container: mapDiv.current,
@@ -169,28 +189,6 @@ function MainMap (props) {
                 view.ui.add(layerList, 'top-right')
             })
 
-            Layer.fromPortalItem({
-                portalItem: {
-                    // autocasts as new PortalItem()
-                    id: 'fbab6ce6b9ea4566a768f3804f4b0bb5'
-                }
-            })
-                .then(addLayer)
-                .catch(rejection)
-
-            // Adds the layer to the map once it loads
-            // eslint-disable-next-line no-inner-declarations
-            function addLayer (layer) {
-                console.log('added')
-                layer.listMode = 'hide'
-                webmap.add(layer)
-            }
-
-            // eslint-disable-next-line no-inner-declarations
-            function rejection (error) {
-                console.log('Layer failed to load: ', error)
-            }
-
             var locate = new Locate({
                 view: view,
                 useHeadingEnabled: false,
@@ -202,8 +200,7 @@ function MainMap (props) {
 
             view.ui.add(locate, 'top-left')
         }
-    }, [])
-
+    }, [layers])
     return (
         <Box className="mapContainer">
             <div style={{ height: window.innerHeight, width: '100%' }} id="mapDiv" ref={mapDiv}></div>
