4d479397f9b851f9dafdd5496d72d9d4ec66f7ae
mathieu.mesquita@rts.ch <mathieu.mesquita@rts.ch>
2021-05-28 15:39:18 +0200

    test og header

--- pages/[events]/dashBoard.js
+++ pages/[events]/dashBoard.js
@@ -33,6 +33,7 @@ import Profile from '../../components/dashboard/profile'
 import ButtonBase from '@material-ui/core/ButtonBase'
 import MapModal from '../../components/map/MapModal'
 import DailyPromoModal from '../../components/dashboard/DailyPromoModal'
+import Head from 'next/head'
 
 const PWAPrompt = dynamic(() => import('react-ios-pwa-prompt'), {
     ssr: false
@@ -257,6 +258,13 @@ function DashBoard (props) {
 
     return (
         <React.Fragment>
+            <Head>
+                <meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+                <meta property="og:type" content="article" />
+                <meta property="og:title" content="When Great Minds Don’t Think Alike" />
+                <meta property="og:description" content="How much does culture influence creative thinking?" />
+                <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+            </Head>
             { openFeedback && <HasTypeFormModal
                 gameStats={gameStats}
                 setOpenFeedback={setOpenFeedback}/> }
